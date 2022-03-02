25bebd72e2abf5a17118698d7eaac92dfd79c67c
Dror Tirosh <dror@opengsn.org>
2022-03-02 15:41:36 +0200

    L12 downcasting without bounds checks (#62)

--- contracts/StakeManager.sol
+++ contracts/StakeManager.sol
@@ -60,11 +60,16 @@ contract StakeManager {
     }
 
     function internalIncrementDeposit(address account, uint amount) internal {
-        deposits[account].amount += uint112(amount);
+        DepositInfo storage info = deposits[account];
+        uint256 newAmount = info.amount + amount;
+        require(newAmount <= type(uint112).max, 'deposit overflow');
+        info.amount = uint112(newAmount);
     }
 
     function internalDecrementDeposit(address account, uint amount) internal {
-        deposits[account].amount -= uint112(amount);
+        DepositInfo storage info = deposits[account];
+        uint256 newAmount = info.amount - amount;
+        info.amount = uint112(newAmount);
     }
 
     /**
