3bf808e6c4558edd8207cb4ccf9252a5db2bd662
Dror Tirosh <dror@opengsn.org>
2022-03-07 17:48:47 +0200

    N16 undocumented assembly (#72)

--- contracts/UserOperation.sol
+++ contracts/UserOperation.sol
@@ -21,8 +21,11 @@ import "hardhat/console.sol";
 
 library UserOperationLib {
 
-    function getSender(UserOperation calldata userOp) internal pure returns (address ret) {
-        assembly {ret := calldataload(userOp)}
+    function getSender(UserOperation calldata userOp) internal pure returns (address) {
+        address data;
+        //read sender from userOp, which is first userOp member (saves 800 gas...)
+        assembly {data := calldataload(userOp)}
+        return address(uint160(data));
     }
 
     //relayer/miner might submit the TX with higher priorityFee, but the user should not
@@ -61,6 +64,9 @@ library UserOperationLib {
     function pack(UserOperation calldata userOp) internal pure returns (bytes memory ret) {
         //lighter signature scheme. must match UserOp.ts#packUserOp
         bytes calldata sig = userOp.signature;
+        // copy directly the userOp from calldata up to (but not including) the signature.
+        // this encoding depends on the ABI encoding of calldata, but is much lighter to copy
+        // than referencing each field separately.
         assembly {
             let ofs := userOp
             let len := sub(sub(sig.offset, ofs), 32)
