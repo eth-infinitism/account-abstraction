1e76671f5bb41ae7536ba6273a2e5c5fb5337cd6
Yoav Weiss <yoavw@users.noreply.github.com>
2022-03-16 00:27:47 +0200

    Changed docstrings

--- contracts/UserOperation.sol
+++ contracts/UserOperation.sol
@@ -10,12 +10,12 @@ import "hardhat/console.sol";
      * @param initCode if set, the account contract will be created by this constructor
      * @param callData the method call to execute on this account.
      * @param verificationGas gas used for validateUserOp and validatePaymasterUserOp
-     * @param preVerificationGas gas not calculated by the handleOps method, but added to the gas paid
+     * @param preVerificationGas gas not calculated by the handleOps method, but added to the gas paid. Covers batch overhead.
      * @param maxFeePerGas same as EIP-1559 gas parameter
      * @param maxPriorityFeePerGas same as EIP-1559 gas parameter
-     * @param paymaster if set, the paymaster will be the transaction instead of the sender
+     * @param paymaster if set, the paymaster will pay for the transaction instead of the sender
      * @param paymasterData extra data used by the paymaster for validation
-     * @param signature sender-verified signature over the entire request
+     * @param signature sender-verified signature over the entire request, the EntryPoint address and the chain ID.
      */
     struct UserOperation {
 
