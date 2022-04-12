a2f4b7be4d9996095e08d7102bacc9f13ea99ff6
Dror Tirosh <dror@opengsn.org>
2022-04-12 13:16:25 +0300

    remove obsolete ECDSA (#89)
    we no longer need to modify ECDSA library (GAS opcode is now allowed
    (just before CALL) on wallet validation code, so no need to work around it)

--- contracts/test/TestUtil.sol
+++ contracts/test/TestUtil.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.12;
 
 import "../UserOperation.sol";
-import "../samples/ECDSA.sol";
 
 contract TestUtil {
     using UserOperationLib for UserOperation;
@@ -13,15 +12,5 @@ contract TestUtil {
 
     function prefund(UserOperation calldata op) public view returns (uint256) {
         return op.requiredPreFund();
-    }
-
-    // helpers for testing ecrecover2
-    // ecrecover2 should be equivalent to "ecrecover()", only not using the "gas" opcode
-    function ecdsa_ecrecover2(bytes32 hash, uint8 v, bytes32 r, bytes32 s) public view returns (address signer) {
-        return ECDSA.ecrecover2(hash, v, r, s);
-    }
-
-    function sol_ecrecover(bytes32 hash, uint8 v, bytes32 r, bytes32 s) public pure returns (address signer) {
-        return ecrecover(hash, v, r, s);
     }
 }
