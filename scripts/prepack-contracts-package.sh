1b2029a4747ab28ae88073b64c3202aaa13830a5
Dror Tirosh <dror@opengsn.org>
2022-08-31 18:04:18 +0300

    update prepack, postpack

--- scripts/prepack-contracts-package.sh
+++ scripts/prepack-contracts-package.sh
@@ -16,4 +16,4 @@ rm -rf artifacts types dist
 mkdir -p artifacts
 cp `find  ../artifacts/contracts -type f | grep -v -E 'Test|dbg|gnosis|bls|IOracle'` artifacts/
 npx typechain --target ethers-v5 --out-dir types  artifacts/**
-tsc index.ts -d --outDir dist
+npx tsc index.ts -d --outDir dist
