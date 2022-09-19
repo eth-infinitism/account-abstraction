533ba074ab7d5c87fc9419436e55894f8196f5aa
Dror Tirosh <dror@opengsn.org>
2022-09-19 21:02:13 +0300

    SimpleWalletDeployer: return address even for existing wallet.

--- contracts/samples/SimpleWalletDeployer.sol
+++ contracts/samples/SimpleWalletDeployer.sol
@@ -4,12 +4,38 @@ pragma solidity ^0.8.12;
 import "./SimpleWallet.sol";
 
 /**
- * a sampler deployer contract for SimpleWallet
- * the "initCode" for a wallet hold its address and a method call (deployWallet) with parameters, not actual constructor code.
+ * A sampler deployer contract for SimpleWallet
+ * A UserOperations "initCode" holds the address of the deployer, and a method call (to deployWallet, in this sample deployer).
+ * The deployer's deployWallet returns the target wallet address even if it is already installed.
+ * This way, the entryPoint.getSenderAddress() can be called either before or after the wallet is created.
  */
 contract SimpleWalletDeployer {
 
-    function deployWallet(IEntryPoint entryPoint, address owner, uint salt) public returns (SimpleWallet) {
-        return new SimpleWallet{salt : bytes32(salt)}(entryPoint, owner);
+    /**
+     * create a wallet, and return its address.
+     * return the address even if the wallet is already deployed.
+     */
+    function deployWallet(IEntryPoint entryPoint, address owner, uint salt) public returns (SimpleWallet ret) {
+        address addr = getAddress(entryPoint, owner, salt);
+        uint codeSize;
+        /* solhint-disable no-inline-assembly */
+        assembly {codeSize := extcodesize(addr)}
+        if (codeSize > 0) {
+            return SimpleWallet(payable(addr));
+        }
+        ret = new SimpleWallet{salt : bytes32(salt)}(entryPoint, owner);
+    }
+
+    /**
+     */
+    function getAddress(IEntryPoint entryPoint, address owner, uint salt) public view returns (address) {
+        return address(uint160(uint256(keccak256(abi.encodePacked(
+                bytes1(0xff),
+                address(this),
+                salt,
+                keccak256(abi.encodePacked(
+                    type(SimpleWallet).creationCode,
+                    abi.encode(entryPoint, owner)
+                )))))));
     }
 }
