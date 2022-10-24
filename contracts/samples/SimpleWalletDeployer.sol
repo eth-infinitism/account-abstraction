18818ca8b98345e87e4a87e36274b77a2858407d
Dror Tirosh <dror@opengsn.org>
2022-10-24 18:44:11 +0300

    deployer: use OZ's Create2 helper to calculate address.

--- contracts/samples/SimpleWalletDeployer.sol
+++ contracts/samples/SimpleWalletDeployer.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.12;
 
 import "./SimpleWallet.sol";
-
+import "@openzeppelin/contracts/utils/Create2.sol";
 /**
  * A sampler deployer contract for SimpleWallet
  * A UserOperations "initCode" holds the address of the deployer, and a method call (to deployWallet, in this sample deployer).
@@ -13,7 +13,9 @@ contract SimpleWalletDeployer {
 
     /**
      * create a wallet, and return its address.
-     * return the address even if the wallet is already deployed.
+     * returns the address even if the wallet is already deployed.
+     * Note that during UserOperation execution, this method is called only if the wallet is not deployed.
+     * This method returns an existing wallet address so that entryPoint.getSenderAddress() would work even after wallet creation
      */
     function deployWallet(IEntryPoint entryPoint, address owner, uint salt) public returns (SimpleWallet ret) {
         address addr = getAddress(entryPoint, owner, salt);
@@ -27,15 +29,12 @@ contract SimpleWalletDeployer {
     }
 
     /**
+     * calculate the counterfactual address of this wallet as it would be returned by deployWallet()
      */
     function getAddress(IEntryPoint entryPoint, address owner, uint salt) public view returns (address) {
-        return address(uint160(uint256(keccak256(abi.encodePacked(
-                bytes1(0xff),
-                address(this),
-                salt,
-                keccak256(abi.encodePacked(
-                    type(SimpleWallet).creationCode,
-                    abi.encode(entryPoint, owner)
-                )))))));
+        return Create2.computeAddress(bytes32(salt), keccak256(abi.encodePacked(
+                type(SimpleWallet).creationCode,
+                abi.encode(entryPoint, owner))
+            ));
     }
 }
