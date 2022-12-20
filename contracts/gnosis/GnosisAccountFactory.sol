d7bd6dc6b32736a13ce422edd453dede9f7ba25e
Dror Tirosh <dror@opengsn.org>
2022-12-20 13:08:03 +0200

    fix gnosis getAddre

--- contracts/gnosis/GnosisAccountFactory.sol
+++ contracts/gnosis/GnosisAccountFactory.sol
@@ -21,11 +21,13 @@ contract GnosisSafeAccountFactory {
     }
 
     function createAccount(address owner, uint salt) public returns (address) {
-        address calc = getAddress(owner, salt);
-        address created = address(proxyFactory.createProxyWithNonce(
+        address addr = getAddress(owner, salt);
+        uint codeSize = addr.code.length;
+        if (codeSize > 0) {
+            return addr;
+        }
+        return address(proxyFactory.createProxyWithNonce(
                 safeSingleton, getInitializer(owner), salt));
-        require(calc == created, "created wrong address");
-        return created;
     }
 
     function getInitializer(address owner) internal view returns (bytes memory) {
@@ -45,35 +47,15 @@ contract GnosisSafeAccountFactory {
             ));
     }
 
-    //an INEFFICIENT way to get the address.
-    // gnosis proxy runs the deployment, and reverts in order to get the address
-    // this also doesn't help if the account is already deployed.
-    // better calculate the real create2 address
-    function getAddress1(address owner, uint salt) public returns (address addr) {
-        try proxyFactory.calculateCreateProxyWithNonceAddress(
-            safeSingleton, getInitializer(owner), salt)
-        // solhint-disable-next-line no-empty-blocks
-        returns (GnosisSafeProxy) {}
-        catch (bytes memory errData) {
-            //Error(string) with 32-byte string
-            require(errData.length == 100, "wrong revert data");
-            /* solhint-disable-next-line no-inline-assembly */
-            assembly {
-                addr := shr(96,mload(sub(add(errData, mload(errData)),0)))
-            }
-            return addr;
-        }
-        revert("never reach here");
-    }
-
     /**
     * calculate the counterfactual address of this account as it would be returned by createAccount()
+    * (uses the same "create2 signature" used by GnosisSafeProxyFactory.createProxyWithNonce)
     */
     function getAddress(address owner, uint salt) public view returns (address) {
         bytes memory initializer = getInitializer(owner);
         //copied from deployProxyWithNonce
         bytes32 salt2 = keccak256(abi.encodePacked(keccak256(initializer), salt));
         bytes memory deploymentData = abi.encodePacked(type(GnosisSafeProxy).creationCode, uint256(uint160(safeSingleton)));
-        return Create2.computeAddress(bytes32(salt2), keccak256(deploymentData));
+        return Create2.computeAddress(bytes32(salt2), keccak256(deploymentData), address (proxyFactory));
     }
 }
