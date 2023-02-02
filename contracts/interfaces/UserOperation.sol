39c5c123bcf6fafc86828c4a4e63053068b8b04a
shahafn <shahaflol@gmail.com>
2023-02-02 18:19:37 +0200

    Fixing docstrings

--- contracts/interfaces/UserOperation.sol
+++ contracts/interfaces/UserOperation.sol
@@ -5,15 +5,16 @@ pragma solidity ^0.8.12;
 
     /**
      * User Operation struct
-     * @param sender the sender account of this request
+     * @param sender the sender account of this request.
      * @param nonce unique value the sender uses to verify it is not a replay.
-     * @param initCode if set, the account contract will be created by this constructor
+     * @param initCode if set, the account contract will be created by this constructor/
      * @param callData the method call to execute on this account.
-     * @param verificationGasLimit gas used for validateUserOp and validatePaymasterUserOp
+     * @param callGasLimit the gas limit passed to the callData method call.
+     * @param verificationGasLimit gas used for validateUserOp and validatePaymasterUserOp.
      * @param preVerificationGas gas not calculated by the handleOps method, but added to the gas paid. Covers batch overhead.
-     * @param maxFeePerGas same as EIP-1559 gas parameter
-     * @param maxPriorityFeePerGas same as EIP-1559 gas parameter
-     * @param paymasterAndData if set, this field hold the paymaster address and "paymaster-specific-data". the paymaster will pay for the transaction instead of the sender
+     * @param maxFeePerGas same as EIP-1559 gas parameter.
+     * @param maxPriorityFeePerGas same as EIP-1559 gas parameter.
+     * @param paymasterAndData if set, this field hold the paymaster address and "paymaster-specific-data". the paymaster will pay for the transaction instead of the sender.
      * @param signature sender-verified signature over the entire request, the EntryPoint address and the chain ID.
      */
     struct UserOperation {
