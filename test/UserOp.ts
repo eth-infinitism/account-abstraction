c1290e72279937b6b95443bc454f5517bbbb5c41
Dror Tirosh <dror@opengsn.org>
2023-03-20 19:15:49 +0200

    getSenderAddress to return nonce

--- test/UserOp.ts
+++ test/UserOp.ts
@@ -4,16 +4,16 @@ import {
   hexDataSlice,
   keccak256
 } from 'ethers/lib/utils'
-import { BigNumber, Contract, Signer, Wallet } from 'ethers'
-import { AddressZero, callDataCost, rethrow } from './testutils'
-import { ecsign, toRpcSig, keccak256 as keccak256_buffer } from 'ethereumjs-util'
+import {BigNumber, Contract, Signer, Wallet} from 'ethers'
+import {AddressZero, callDataCost, rethrow} from './testutils'
+import {ecsign, toRpcSig, keccak256 as keccak256_buffer} from 'ethereumjs-util'
 import {
   EntryPoint
 } from '../typechain'
-import { UserOperation } from './UserOperation'
-import { Create2Factory } from '../src/Create2Factory'
+import {UserOperation} from './UserOperation'
+import {Create2Factory} from '../src/Create2Factory'
 
-function encode (typevalues: Array<{ type: string, val: any }>, forSignature: boolean): string {
+function encode(typevalues: Array<{ type: string, val: any }>, forSignature: boolean): string {
   const types = typevalues.map(typevalue => typevalue.type === 'bytes' && forSignature ? 'bytes32' : typevalue.type)
   const values = typevalues.map((typevalue) => typevalue.type === 'bytes' && forSignature ? keccak256(typevalue.val) : typevalue.val)
   return defaultAbiCoder.encode(types, values)
@@ -31,51 +31,51 @@ function encode (typevalues: Array<{ type: string, val: any }>, forSignature: bo
 //   return packed
 // }
 
-export function packUserOp (op: UserOperation, forSignature = true): string {
+export function packUserOp(op: UserOperation, forSignature = true): string {
   if (forSignature) {
     // lighter signature scheme (must match UserOperation#pack): do encode a zero-length signature, but strip afterwards the appended zero-length value
     const userOpType = {
       components: [
-        { type: 'address', name: 'sender' },
-        { type: 'uint256', name: 'nonce' },
-        { type: 'bytes', name: 'initCode' },
-        { type: 'bytes', name: 'callData' },
-        { type: 'uint256', name: 'callGasLimit' },
-        { type: 'uint256', name: 'verificationGasLimit' },
-        { type: 'uint256', name: 'preVerificationGas' },
-        { type: 'uint256', name: 'maxFeePerGas' },
-        { type: 'uint256', name: 'maxPriorityFeePerGas' },
-        { type: 'bytes', name: 'paymasterAndData' },
-        { type: 'bytes', name: 'signature' }
+        {type: 'address', name: 'sender'},
+        {type: 'uint256', name: 'nonce'},
+        {type: 'bytes', name: 'initCode'},
+        {type: 'bytes', name: 'callData'},
+        {type: 'uint256', name: 'callGasLimit'},
+        {type: 'uint256', name: 'verificationGasLimit'},
+        {type: 'uint256', name: 'preVerificationGas'},
+        {type: 'uint256', name: 'maxFeePerGas'},
+        {type: 'uint256', name: 'maxPriorityFeePerGas'},
+        {type: 'bytes', name: 'paymasterAndData'},
+        {type: 'bytes', name: 'signature'}
       ],
       name: 'userOp',
       type: 'tuple'
     }
-    let encoded = defaultAbiCoder.encode([userOpType as any], [{ ...op, signature: '0x' }])
+    let encoded = defaultAbiCoder.encode([userOpType as any], [{...op, signature: '0x'}])
     // remove leading word (total length) and trailing word (zero-length signature)
     encoded = '0x' + encoded.slice(66, encoded.length - 64)
     return encoded
   }
   const typevalues = [
-    { type: 'address', val: op.sender },
-    { type: 'uint256', val: op.nonce },
-    { type: 'bytes', val: op.initCode },
-    { type: 'bytes', val: op.callData },
-    { type: 'uint256', val: op.callGasLimit },
-    { type: 'uint256', val: op.verificationGasLimit },
-    { type: 'uint256', val: op.preVerificationGas },
-    { type: 'uint256', val: op.maxFeePerGas },
-    { type: 'uint256', val: op.maxPriorityFeePerGas },
-    { type: 'bytes', val: op.paymasterAndData }
+    {type: 'address', val: op.sender},
+    {type: 'uint256', val: op.nonce},
+    {type: 'bytes', val: op.initCode},
+    {type: 'bytes', val: op.callData},
+    {type: 'uint256', val: op.callGasLimit},
+    {type: 'uint256', val: op.verificationGasLimit},
+    {type: 'uint256', val: op.preVerificationGas},
+    {type: 'uint256', val: op.maxFeePerGas},
+    {type: 'uint256', val: op.maxPriorityFeePerGas},
+    {type: 'bytes', val: op.paymasterAndData}
   ]
   if (!forSignature) {
     // for the purpose of calculating gas cost, also hash signature
-    typevalues.push({ type: 'bytes', val: op.signature })
+    typevalues.push({type: 'bytes', val: op.signature})
   }
   return encode(typevalues, forSignature)
 }
 
-export function packUserOp1 (op: UserOperation): string {
+export function packUserOp1(op: UserOperation): string {
   return defaultAbiCoder.encode([
     'address', // sender
     'uint256', // nonce
@@ -101,7 +101,7 @@ export function packUserOp1 (op: UserOperation): string {
   ])
 }
 
-export function getUserOpHash (op: UserOperation, entryPoint: string, chainId: number): string {
+export function getUserOpHash(op: UserOperation, entryPoint: string, chainId: number): string {
   const userOpHash = keccak256(packUserOp(op, true))
   const enc = defaultAbiCoder.encode(
     ['bytes32', 'address', 'uint256'],
@@ -115,7 +115,7 @@ export const DefaultsForUserOp: UserOperation = {
   initCode: '0x',
   callData: '0x',
   callGasLimit: 0,
-  verificationGasLimit: 100000, // default verification gas. will add create2 cost (3200+200*length) if initCode exists
+  verificationGasLimit: 150000, // default verification gas. will add create2 cost (3200+200*length) if initCode exists
   preVerificationGas: 21000, // should also cover calldata cost.
   maxFeePerGas: 0,
   maxPriorityFeePerGas: 1e9,
@@ -123,7 +123,7 @@ export const DefaultsForUserOp: UserOperation = {
   signature: '0x'
 }
 
-export function signUserOp (op: UserOperation, signer: Wallet, entryPoint: string, chainId: number): UserOperation {
+export function signUserOp(op: UserOperation, signer: Wallet, entryPoint: string, chainId: number): UserOperation {
   const message = getUserOpHash(op, entryPoint, chainId)
   const msg1 = Buffer.concat([
     Buffer.from('\x19Ethereum Signed Message:\n32', 'ascii'),
@@ -140,8 +140,8 @@ export function signUserOp (op: UserOperation, signer: Wallet, entryPoint: strin
   }
 }
 
-export function fillUserOpDefaults (op: Partial<UserOperation>, defaults = DefaultsForUserOp): UserOperation {
-  const partial: any = { ...op }
+export function fillUserOpDefaults(op: Partial<UserOperation>, defaults = DefaultsForUserOp): UserOperation {
+  const partial: any = {...op}
   // we want "item:undefined" to be used from defaults, and not override defaults, so we must explicitly
   // remove those so "merge" will succeed.
   for (const key in partial) {
@@ -150,7 +150,7 @@ export function fillUserOpDefaults (op: Partial<UserOperation>, defaults = Defau
       delete partial[key]
     }
   }
-  const filled = { ...defaults, ...partial }
+  const filled = {...defaults, ...partial}
   return filled
 }
 
@@ -166,24 +166,26 @@ export function fillUserOpDefaults (op: Partial<UserOperation>, defaults = Defau
 // sender - only in case of construction: fill sender from initCode.
 // callGasLimit: VERY crude estimation (by estimating call to account, and add rough entryPoint overhead
 // verificationGasLimit: hard-code default at 100k. should add "create2" cost
-export async function fillUserOp (op: Partial<UserOperation>, entryPoint?: EntryPoint): Promise<UserOperation> {
-  const op1 = { ...op }
+export async function fillUserOp(op: Partial<UserOperation>, entryPoint?: EntryPoint, getNonceFunction = 'nonce'): Promise<UserOperation> {
+  const op1 = {...op}
   const provider = entryPoint?.provider
   if (op.initCode != null) {
     const initAddr = hexDataSlice(op1.initCode!, 0, 20)
     const initCallData = hexDataSlice(op1.initCode!, 20)
-    if (op1.nonce == null) op1.nonce = 0
-    if (op1.sender == null) {
-      // hack: if the init contract is our known deployer, then we know what the address would be, without a view call
-      if (initAddr.toLowerCase() === Create2Factory.contractAddress.toLowerCase()) {
-        const ctr = hexDataSlice(initCallData, 32)
-        const salt = hexDataSlice(initCallData, 0, 32)
-        op1.sender = Create2Factory.getDeployedAddress(ctr, salt)
+    if (op1.sender == null || op1.nonce == null) {
+
+      if (provider == null) throw new Error('no entrypoint/provider')
+      const {
+        sender,
+        nonce
+      } = await entryPoint!.callStatic.getSenderAddress(op1.initCode!).catch(e => e.errorArgs)
+      if (op1.sender == null ) {
+        op1.sender = sender
       } else {
-        // console.log('\t== not our deployer. our=', Create2Factory.contractAddress, 'got', initAddr)
-        if (provider == null) throw new Error('no entrypoint/provider')
-        op1.sender = await entryPoint!.callStatic.getSenderAddress(op1.initCode!).catch(e => e.errorArgs.sender)
+        //should check that sender == op1.sender.
+        // but we use this fill method in tests to test explicitly the on-chain handling of broken sender
       }
+      op1.nonce = nonce
     }
     if (op1.verificationGasLimit == null) {
       if (provider == null) throw new Error('no entrypoint/provider')
@@ -198,8 +200,9 @@ export async function fillUserOp (op: Partial<UserOperation>, entryPoint?: Entry
   }
   if (op1.nonce == null) {
     if (provider == null) throw new Error('must have entryPoint to autofill nonce')
-    const c = new Contract(op.sender!, ['function nonce() view returns(address)'], provider)
-    op1.nonce = await c.nonce().catch(rethrow())
+    const c = new Contract(op.sender!, [`function ${getNonceFunction}() view returns(address)`], provider)
+    op1.nonce = await c[getNonceFunction]().catch(rethrow())
+    console.log('nonce=', op1.nonce)
   }
   if (op1.callGasLimit == null && op.callData != null) {
     if (provider == null) throw new Error('must have entryPoint for callGasLimit estimate')
@@ -232,9 +235,9 @@ export async function fillUserOp (op: Partial<UserOperation>, entryPoint?: Entry
   return op2
 }
 
-export async function fillAndSign (op: Partial<UserOperation>, signer: Wallet | Signer, entryPoint?: EntryPoint): Promise<UserOperation> {
+export async function fillAndSign(op: Partial<UserOperation>, signer: Wallet | Signer, entryPoint?: EntryPoint, getNonceFunction = 'nonce'): Promise<UserOperation> {
   const provider = entryPoint?.provider
-  const op2 = await fillUserOp(op, entryPoint)
+  const op2 = await fillUserOp(op, entryPoint, getNonceFunction)
 
   const chainId = await provider!.getNetwork().then(net => net.chainId)
   const message = arrayify(getUserOpHash(op2, entryPoint!.address, chainId))
--- test/simple-wallet.test.ts
+++ test/simple-wallet.test.ts
@@ -1,32 +1,37 @@
-import { Wallet } from 'ethers'
-import { ethers } from 'hardhat'
-import { expect } from 'chai'
+import {Wallet} from 'ethers'
+import {ethers} from 'hardhat'
+import {expect} from 'chai'
 import {
+  ERC1967Proxy__factory,
   SimpleAccount,
   SimpleAccountFactory__factory,
+  SimpleAccount__factory,
   TestUtil,
   TestUtil__factory
 } from '../typechain'
 import {
+  createAccount,
   createAddress,
   createAccountOwner,
+  deployEntryPoint,
   getBalance,
   isDeployed,
   ONE_ETH,
-  createAccount, HashZero
+  HashZero,
 } from './testutils'
-import { fillUserOpDefaults, getUserOpHash, packUserOp, signUserOp } from './UserOp'
-import { parseEther } from 'ethers/lib/utils'
-import { UserOperation } from './UserOperation'
+import {fillUserOpDefaults, getUserOpHash, packUserOp, signUserOp} from './UserOp'
+import {parseEther} from 'ethers/lib/utils'
+import {UserOperation} from './UserOperation'
 
 describe('SimpleAccount', function () {
-  const entryPoint = '0x'.padEnd(42, '2')
+  let entryPoint: string
   let accounts: string[]
   let testUtil: TestUtil
   let accountOwner: Wallet
   const ethersSigner = ethers.provider.getSigner()
 
   before(async function () {
+    entryPoint = await deployEntryPoint().then(e => e.address)
     accounts = await ethers.provider.listAccounts()
     // ignore in geth.. this is just a sanity test. should be refactored to use a single-account mode..
     if (accounts.length < 2) this.skip()
@@ -35,18 +40,18 @@ describe('SimpleAccount', function () {
   })
 
   it('owner should be able to call transfer', async () => {
-    const { proxy: account } = await createAccount(ethers.provider.getSigner(), accounts[0], entryPoint)
-    await ethersSigner.sendTransaction({ from: accounts[0], to: account.address, value: parseEther('2') })
+    const {proxy: account} = await createAccount(ethers.provider.getSigner(), accounts[0], entryPoint)
+    await ethersSigner.sendTransaction({from: accounts[0], to: account.address, value: parseEther('2')})
     await account.execute(accounts[2], ONE_ETH, '0x')
   })
   it('other account should not be able to call transfer', async () => {
-    const { proxy: account } = await createAccount(ethers.provider.getSigner(), accounts[0], entryPoint)
+    const {proxy: account} = await createAccount(ethers.provider.getSigner(), accounts[0], entryPoint)
     await expect(account.connect(ethers.provider.getSigner(1)).execute(accounts[2], ONE_ETH, '0x'))
       .to.be.revertedWith('account: not Owner or EntryPoint')
   })
 
   it('should pack in js the same as solidity', async () => {
-    const op = await fillUserOpDefaults({ sender: accounts[0] })
+    const op = await fillUserOpDefaults({sender: accounts[0]})
     const packed = packUserOp(op)
     expect(await testUtil.packUserOp(op)).to.equal(packed)
   })
@@ -59,12 +64,21 @@ describe('SimpleAccount', function () {
     let expectedPay: number
 
     const actualGasPrice = 1e9
+    //for testing directly validateUserOp, we initialize the account with EOA as entryPoint.
+    let entryPointEoa: string
 
     before(async () => {
-      // that's the account of ethersSigner
-      const entryPoint = accounts[2];
-      ({ proxy: account } = await createAccount(await ethers.getSigner(entryPoint), accountOwner.address, entryPoint))
-      await ethersSigner.sendTransaction({ from: accounts[0], to: account.address, value: parseEther('0.2') })
+      entryPointEoa = accounts[2];
+      let epAsSigner = await ethers.getSigner(entryPointEoa);
+      const impl = await new SimpleAccount__factory(await epAsSigner).deploy(entryPointEoa)
+      // ({ proxy: account } = await createAccount(await ethers.getSigner(entryPoint), accountOwner.address, entryPoint))
+
+      // cant use "SimpleAccountFactory", since it attempts to increment nonce first
+      const implementation = await new SimpleAccount__factory(ethersSigner).deploy(entryPointEoa)
+      const proxy = await new ERC1967Proxy__factory(ethersSigner).deploy(implementation.address, "0x")
+      account = SimpleAccount__factory.connect(proxy.address, epAsSigner)
+
+      await ethersSigner.sendTransaction({from: accounts[0], to: account.address, value: parseEther('0.2')})
       const callGasLimit = 200000
       const verificationGasLimit = 100000
       const maxFeePerGas = 3e9
@@ -75,14 +89,14 @@ describe('SimpleAccount', function () {
         callGasLimit,
         verificationGasLimit,
         maxFeePerGas
-      }), accountOwner, entryPoint, chainId)
+      }), accountOwner, entryPointEoa, chainId)
 
-      userOpHash = await getUserOpHash(userOp, entryPoint, chainId)
+      userOpHash = await getUserOpHash(userOp, entryPointEoa, chainId)
 
       expectedPay = actualGasPrice * (callGasLimit + verificationGasLimit)
 
       preBalance = await getBalance(account.address)
-      const ret = await account.validateUserOp(userOp, userOpHash, expectedPay, { gasPrice: actualGasPrice })
+      const ret = await account.validateUserOp(userOp, userOpHash, expectedPay, {gasPrice: actualGasPrice})
       await ret.wait()
     })
 
@@ -93,10 +107,11 @@ describe('SimpleAccount', function () {
 
     it('should return NO_SIG_VALIDATION on wrong signature', async () => {
       const userOpHash = HashZero
-      const deadline = await account.callStatic.validateUserOp({ ...userOp, nonce: 1 }, userOpHash, 0)
+      const deadline = await account.callStatic.validateUserOp({...userOp, nonce: 1}, userOpHash, 0)
       expect(deadline).to.eq(1)
     })
   })
+
   context('SimpleAccountFactory', () => {
     it('sanity: check deployer', async () => {
       const ownerAddr = createAddress()
