431787cc0a9e32fd8967020aaaa4ffb3fefb4d7c
SHØ <neila@users.noreply.github.com>
2023-03-22 11:08:19 +0900

    add precommit hook

---
+++ .lintstagedrc.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const buildLintCommand = (filenames) =>
+  `eslint -f unix ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' ')}`;
+
+const buildSolhintCommand = (filenames) =>
+  `solhint -f unix --max-warnings 0 ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' ')} `;
+
+const buildPrettierCommand = (filenames) =>
+  `prettier --check ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' ')} `;
+
+module.exports = {
+  '**/*.{js,jsx,ts,tsx}': [buildLintCommand],
+  '**/*.{js,jsx,ts,tsx,sol}': [buildPrettierCommand],
+  '**/*.sol': [buildSolhintCommand],
+};
