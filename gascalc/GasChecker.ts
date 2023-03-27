bd8e11437d21c26968193b972ff511724a7d041a
Dror Tirosh <dror.tirosh@gmail.com>
2023-03-27 14:10:25 +0300

    Re-base gascheck (#252)
    Change the base for "gascheck"
    This is not a change in the contracts, but in the scripts to calculate the gas usage.
    The default sample call now calls entryPoint() in the account, which returns a constant (entrypoint) instead of a variable (nonce)
    The 2000 extra gas overhead seen results/gas-checker.txt in gas is actually a fix of a mistake: previously, accessing the "nonce" field was attributed to the "exec" call (because we called "nonce()"). Now it is rightfully attributed to the validation, not execution.

--- gascalc/GasChecker.ts
+++ gascalc/GasChecker.ts
@@ -52,7 +52,7 @@ interface GasTestInfo {
 export const DefaultGasTestInfo: Partial<GasTestInfo> = {
   dest: 'self', // destination is the account itself.
   destValue: parseEther('0'),
-  destCallData: '0xaffed0e0', // nonce()
+  destCallData: '0xb0d691fe', // entryPoint()
   gasPrice: 10e9
 }
 
@@ -338,15 +338,17 @@ export class GasCheckCollector {
       fs.appendFileSync(gasCheckerLogFile, s + '\n')
     }
 
-    write('== gas estimate of direct calling the account\'s "execFromEntryPoint" method')
-    write('   the destination is "account.nonce()", which is known to be "hot" address used by this account')
+    write('== gas estimate of direct calling the account\'s "execute" method')
+    write('   the destination is "account.entryPoint()", which is known to be "hot" address used by this account')
     write('   it little higher than EOA call: its an exec from entrypoint (or account owner) into account contract, verifying msg.sender and exec to target)')
-    Object.values(gasEstimatePerExec).forEach(({ title, accountEst }) => {
-      write(`- gas estimate "${title}" - ${accountEst}`)
-    })
+
+    write(table(Object.values(gasEstimatePerExec).map((row) => [
+      `gas estimate "${row.title}"`, row.accountEst
+    ]), this.tableConfig))
 
     const tableOutput = table(this.tabRows, this.tableConfig)
     write(tableOutput)
+    process.exit(0)
   }
 
   addRow (res: GasTestResult): void {
