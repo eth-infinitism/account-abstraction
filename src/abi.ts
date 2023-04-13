811a1f242eff7cb567527dbdf150267017c7a2f8
SHØ <neila@users.noreply.github.com>
2023-04-13 09:28:22 +0900

    add src scripts

---
+++ src/abi.ts
@@ -0,0 +1,12 @@
+export const ERC20_ABI = [
+  // Read-Only Functions
+  "function balanceOf(address owner) view returns (uint256)",
+  "function decimals() view returns (uint8)",
+  "function symbol() view returns (string)",
+
+  // Authenticated Functions
+  "function transfer(address to, uint amount) returns (bool)",
+
+  // Events
+  "event Transfer(address indexed from, address indexed to, uint amount)",
+];
