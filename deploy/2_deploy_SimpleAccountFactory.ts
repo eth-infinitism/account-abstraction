e7a2efc8c21c3d03017b398497b58d818f4849cb
Dror Tirosh <dror.tirosh@gmail.com>
2023-04-24 19:32:00 +0300

    deploy script: deploy factory only on test networks. (#260)
    * deploy script: deploy factory only on test networks.

--- deploy/2_deploy_SimpleAccountFactory.ts
+++ deploy/2_deploy_SimpleAccountFactory.ts
@@ -5,6 +5,11 @@ import { ethers } from 'hardhat'
 const deploySimpleAccountFactory: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const provider = ethers.provider
   const from = await provider.getSigner().getAddress()
+  const network = await provider.getNetwork()
+  // only deploy on local test network.
+  if (network.chainId !== 31337 && network.chainId !== 1337) {
+    return
+  }
 
   const entrypoint = await hre.deployments.get('EntryPoint')
   const ret = await hre.deployments.deploy(
