7f6c103b0f109583b361b746f49c5938fd075481
Alex Forshtat <forshtat1@gmail.com>
2023-08-15 13:43:58 +0200

    Fix EntryPoint revert if postOp reverts with short revert reason (#308)
    * Makes comments and formatting consistent
    
    * @inheritdoc IEntryPoint
    
    * Reverts contracts/test/TestHelpers.sol
    
    * fix: entrypoint does not revert even first postOp reverts with short revert reason
    
    * lint
    
    * gas-report
    
    * Update IEntryPoint.sol - add missing 'INonceManager'
    
    * Update EntryPoint.sol - fix lint errors
    
    * Update gas-checker.txt manually
    
    * Update gas-checker.txt - add newline for the 'diff'
    
    * Update gas-checker.txt manually
    
    ---------
    
    Co-authored-by: Matt Masurka <m.masurka@gmail.com>
    Co-authored-by: leekt <leekt216@gmail.com>

---
+++ contracts/test/TestPaymasterRevertCustomError.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.12;
+
+import "../core/BasePaymaster.sol";
+
+/**
+ * test postOp revert with custom error
+ */
+error CustomError();
+
+contract TestPaymasterRevertCustomError is BasePaymaster {
+    // solhint-disable no-empty-blocks
+    constructor(IEntryPoint _entryPoint) BasePaymaster(_entryPoint)
+    {}
+
+    function _validatePaymasterUserOp(UserOperation calldata userOp, bytes32, uint256)
+    internal virtual override view
+    returns (bytes memory context, uint256 validationData) {
+        validationData = 0;
+        context = abi.encodePacked(userOp.sender);
+    }
+
+    function _postOp(PostOpMode mode, bytes calldata, uint256) internal override {
+        if(mode == PostOpMode.postOpReverted) {
+            return;
+        }
+
+        revert CustomError();
+    }
+}
