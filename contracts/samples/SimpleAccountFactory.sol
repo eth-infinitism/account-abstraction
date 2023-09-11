d629dd8dc200afa0bea33837a6f81821d4d26ff8
Dror Tirosh <dror.tirosh@gmail.com>
2023-09-12 02:16:32 +0300

    remove console.log

--- contracts/samples/SimpleAccountFactory.sol
+++ contracts/samples/SimpleAccountFactory.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/utils/Create2.sol";
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 import "./SimpleAccount.sol";
-import "hardhat/console.sol";
 
 /**
  * A sample factory contract for SimpleAccount
