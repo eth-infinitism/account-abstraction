83cb4e2ead0cef64c6490ee6890a57e2ff625dc7
Alex Forshtat <forshtat1@gmail.com>
2023-10-18 19:53:11 +0300

    Uncomment TestCounter deployment

--- deploy/1_deploy_entrypoint.ts
+++ deploy/1_deploy_entrypoint.ts
@@ -16,7 +16,7 @@ const deployEntryPoint: DeployFunction = async function (hre: HardhatRuntimeEnvi
       deterministicDeployment: true
     })
   console.log('==entrypoint addr=', ret.address)
-/*
+
   const entryPointAddress = ret.address
   const w = await hre.deployments.deploy(
     'SimpleAccount', {
@@ -33,7 +33,6 @@ const deployEntryPoint: DeployFunction = async function (hre: HardhatRuntimeEnvi
     deterministicDeployment: true
   })
   console.log('==testCounter=', t.address)
-  */
 }
 
 export default deployEntryPoint
