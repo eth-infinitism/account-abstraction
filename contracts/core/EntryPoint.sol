b6a2a64c73e809ab6428cf35e85c2637a15383f4
Alex Forshtat <forshtat1@gmail.com>
2023-10-18 23:16:52 +0300

    AA-217: Charge a penalty payment for unused execution gas limit of a UserOp

--- contracts/core/EntryPoint.sol
+++ contracts/core/EntryPoint.sol
@@ -19,6 +19,8 @@ import "./UserOperationLib.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol" as OpenZeppelin;
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
+import "hardhat/console.sol";
+
 /*
  * Account-Abstraction (EIP-4337) singleton EntryPoint implementation.
  * Only one instance required on each chain.
@@ -33,6 +35,7 @@ contract EntryPoint is IEntryPoint, StakeManager, NonceManager, ReentrancyGuard,
     bytes32 private constant INNER_OUT_OF_GAS = hex"deaddead";
 
     uint256 private constant REVERT_REASON_MAX_LEN = 2048;
+    uint256 private constant PENALTY_PERCENT = 10;
 
     /**
      * For simulation purposes, validateUserOp (and validatePaymasterUserOp)
@@ -668,6 +671,20 @@ contract EntryPoint is IEntryPoint, StakeManager, NonceManager, ReentrancyGuard,
                 }
             }
             actualGas += preGas - gasleft();
+
+            // Calculating a penalty for unused execution gas
+            {
+                uint256 executionGasLimit = mUserOp.callGasLimit;
+                // Note that 'verificationGasLimit' here is the limit given to the 'postOp' which is part of execution
+                if (context.length > 0){
+                    executionGasLimit += mUserOp.verificationGasLimit;
+                }
+                uint256 executionGasUsed = actualGas - opInfo.preOpGas;
+                uint256 unusedGas = executionGasLimit - executionGasUsed;
+                uint256 unusedGasPenalty = (unusedGas * PENALTY_PERCENT) / 100;
+                actualGas += unusedGasPenalty;
+            }
+
             actualGasCost = actualGas * gasPrice;
             if (opInfo.prefund < actualGasCost) {
                 revert FailedOp(opIndex, "AA51 prefund below actualGasCost");
