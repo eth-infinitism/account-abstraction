0524529465d917f08c1137e63a78c5f17f336b6b
shahafn <shahaflol@gmail.com>
2024-01-02 11:19:12 +0200

    AA-238 Removing obsolete gnosis safe 4337 module (#389)
    * Removing obsolete gnosis safe module

--- scripts/prepack-contracts-package.sh
+++ scripts/prepack-contracts-package.sh
@@ -7,13 +7,13 @@ if git status contracts | grep -v 'nothing to commit'|tee /dev/stderr |grep -q U
   exit 1
 fi
 
-yarn clean 
+yarn clean
 yarn compile
 cd contracts
 
 rm -rf artifacts types dist
 
 mkdir -p artifacts
-cp `find  ../artifacts/contracts -type f | grep -v -E 'Test|dbg|gnosis|bls|IOracle'` artifacts/
+cp `find  ../artifacts/contracts -type f | grep -v -E 'Test|dbg|bls|IOracle'` artifacts/
 npx typechain --target ethers-v5 --out-dir types  artifacts/**
 npx tsc index.ts -d --outDir dist
