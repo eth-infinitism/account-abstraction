b92deca9b84afe13f1494cdc4b23688254418a7c
Dror Tirosh <dror.tirosh@gmail.com>
2024-01-03 16:15:30 +0200

    add separate test for paymaster with postOp (#391)
    * add separate test for paymaster with postOp
    
    we use our "gas tests" to check the impact of specific version.
    it is important to to break tests.
    This PR make "simple paymaster" simple again, and adds a separte test of
    "paymaster with postOp"
    
    tests are ordered so that they can be directly diff'ed with (say) v0.6.0
    contracts
    * separate TestPaymasterWithPostOp

---
+++ contracts/test/TestPaymasterWithPostOp.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.12;
+
+import "./TestPaymasterAcceptAll.sol";
+/* solhint-disable no-empty-blocks */
+
+/**
+ * test paymaster, that pays for everything, without any check.
+ * explicitly returns a context, to test cost (for entrypoint) to call postOp
+ */
+contract TestPaymasterWithPostOp is TestPaymasterAcceptAll {
+
+    constructor(IEntryPoint _entryPoint) TestPaymasterAcceptAll(_entryPoint) {
+    }
+
+    function _validatePaymasterUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256 maxCost)
+    internal virtual override view
+    returns (bytes memory context, uint256 validationData) {
+        (userOp, userOpHash, maxCost);
+        // return a context, to force a call for postOp.
+        return ("1", 0);
+    }
+
+    function _postOp(
+        PostOpMode mode,
+        bytes calldata context,
+        uint256 actualGasCost
+    ) internal override {
+    }
+}
