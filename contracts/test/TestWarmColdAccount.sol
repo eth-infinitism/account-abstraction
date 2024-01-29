5702b92343887d56604db8ac5d199bdf6c836425
Dror Tirosh <dror.tirosh@gmail.com>
2024-01-29 19:18:19 +0200

    update sig_validation_success

--- contracts/test/TestWarmColdAccount.sol
+++ contracts/test/TestWarmColdAccount.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.12;
 
 import "../interfaces/IEntryPoint.sol";
 import "../interfaces/IAccount.sol";
+import "../core/Helpers.sol";
 
 // Using eip-2929 (https://eips.ethereum.org/EIPS/eip-2929) warm/cold storage access gas costs to detect simulation vs execution
 // COLD_ACCOUNT_ACCESS_COST == 2600, COLD_SLOAD_COST == 2100, WARM_STORAGE_READ_COST == 100
@@ -25,7 +26,7 @@ contract TestWarmColdAccount is IAccount {
             // can only succeed if storage is already warm
             this.touchPaymaster{gas: 1000}(paymaster);
         }
-        return 0;
+        return SIG_VALIDATION_SUCCESS;
     }
 
     function touchStorage() public view returns (uint256) {
