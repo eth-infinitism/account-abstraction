be244c43b1a92b39f9e44f437c4ec5546b52fdfb
Alex Forshtat <forshtat1@gmail.com>
2024-12-25 12:51:57 +0100

    UNDO changes to EPS test

--- test/entrypointsimulations.test.ts
+++ test/entrypointsimulations.test.ts
@@ -16,13 +16,10 @@ import {
   createAccount,
   createAccountOwner,
   createAddress,
-  decodeRevertReason,
-  deployEntryPoint,
-  findSimulationUserOpWithMin,
   fund,
   getAccountAddress,
   getAccountInitCode,
-  getBalance
+  getBalance, deployEntryPoint, decodeRevertReason, findSimulationUserOpWithMin, findUserOpWithMin
 } from './testutils'
 
 import { fillAndSign, fillSignAndPack, packUserOp, simulateHandleOp, simulateValidation } from './UserOp'
@@ -293,6 +290,27 @@ describe('EntryPointSimulations', function () {
             throw new Error(decodeRevertReason(e)!)
           })
       })
+
+      describe(`compare to execution ${withPaymaster} paymaster`, () => {
+        let execVgl: number
+        let execPmVgl: number
+        const diff = 2000
+        before(async () => {
+          execPmVgl = withPaymaster === 'without' ? 0 : await findUserOpWithMin(async n => userOpWithGas(1e6, n), false, entryPoint, 1, 500000)
+          execVgl = await findUserOpWithMin(async n => userOpWithGas(n, execPmVgl), false, entryPoint, 1, 500000)
+        })
+        it(`account verification simulation cost should be higher than execution ${withPaymaster} paymaster`, function () {
+          console.log('simulation account validation', vgl, 'above exec:', vgl - execVgl)
+          expect(vgl).to.be.within(execVgl + 1, execVgl + diff, `expected simulation verificationGas to be 1..${diff} above actual, but was ${vgl - execVgl}`)
+        })
+        if (withPaymaster === 'with') {
+          it('paymaster verification simulation cost should be higher than execution', function () {
+            console.log('simulation paymaster validation', pmVgl, 'above exec:', pmVgl - execPmVgl)
+            expect(pmVgl).to.be.within(execPmVgl + 1, execPmVgl + diff, `expected simulation verificationGas to be 1..${diff} above actual, but was ${pmVgl - execPmVgl}`)
+          })
+        }
+      })
+
       it('should revert with AA2x if verificationGasLimit is low', async function () {
         expect(await simulateValidation(packUserOp(await userOpWithGas(vgl - 1, pmVgl)), entryPoint.address)
           .catch(decodeRevertReason))
