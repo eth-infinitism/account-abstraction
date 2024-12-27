7f6f8164795a056164f380d2aea481b8fc835559
Alex Forshtat <forshtat1@gmail.com>
2024-12-27 14:32:11 +0100

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -59,9 +59,6 @@ const config: HardhatUserConfig = {
     }
   },
   networks: {
-    hardhat: {
-      allowUnlimitedContractSize: true
-    },
     dev: { url: 'http://localhost:8545' },
     // github action starts localgeth service, for gas calculations
     localgeth: { url: 'http://localgeth:8545' },
