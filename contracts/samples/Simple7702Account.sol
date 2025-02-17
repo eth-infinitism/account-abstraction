e3f4810cf5b2cef634e6f257bcdbcf452286bcc9
shahafn <shahaflol@gmail.com>
2025-02-17 17:48:17 +0200

    Updating EP address

--- contracts/samples/Simple7702Account.sol
+++ contracts/samples/Simple7702Account.sol
@@ -17,7 +17,7 @@ contract Simple7702Account is BaseAccount, IERC165, IERC1271, ERC1155Holder, ERC
 
     // temporary address of entryPoint v0.8
     function entryPoint() public pure override returns (IEntryPoint) {
-        return IEntryPoint(0x89b1496Eed8aB8Accb164e3374BF98731A6216cd);
+        return IEntryPoint(0x0839a9d6F1a64d912726D118D932C8336f7C9864);
     }
 
     /**
