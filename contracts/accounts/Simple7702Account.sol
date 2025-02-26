3f66f14a54abb00445f2e51f97200aa6766bb2a1
shahafn <shahaflol@gmail.com>
2025-02-27 00:44:44 +0200

    Fixing EntryPoint address, gas checks

--- contracts/accounts/Simple7702Account.sol
+++ contracts/accounts/Simple7702Account.sol
@@ -17,7 +17,7 @@ contract Simple7702Account is BaseAccount, IERC165, IERC1271, ERC1155Holder, ERC
 
     // temporary address of entryPoint v0.8
     function entryPoint() public pure override returns (IEntryPoint) {
-        return IEntryPoint(0x9aeC529Bed949D43BB3dFedc69A3BA9f8FEE1018);
+        return IEntryPoint(0xd50BE0da15e04EF4fADbcbA27642179231E0A862);
     }
 
     /**
