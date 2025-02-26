5d77f71c13cda7f435d0ece35484b5a2bbc80e5c
Alex Forshtat <forshtat1@gmail.com>
2025-02-26 13:02:30 +0100

    Update EntryPoint address

--- contracts/accounts/Simple7702Account.sol
+++ contracts/accounts/Simple7702Account.sol
@@ -17,7 +17,7 @@ contract Simple7702Account is BaseAccount, IERC165, IERC1271, ERC1155Holder, ERC
 
     // temporary address of entryPoint v0.8
     function entryPoint() public pure override returns (IEntryPoint) {
-        return IEntryPoint(0x97912c962F6Abb5E87b6037686A054F82266f360);
+        return IEntryPoint(0x296D36ed33D6650796c4C89Be35E937FF02558C5);
     }
 
     /**
