926e486cc9ddf771b70343b0d64f28915e6871f2
Alex Forshtat <forshtat1@gmail.com>
2025-03-02 18:10:07 +0100

    Fix address and gas check

--- contracts/accounts/Simple7702Account.sol
+++ contracts/accounts/Simple7702Account.sol
@@ -17,7 +17,7 @@ contract Simple7702Account is BaseAccount, IERC165, IERC1271, ERC1155Holder, ERC
 
     // temporary address of entryPoint v0.8
     function entryPoint() public pure override returns (IEntryPoint) {
-        return IEntryPoint(0x8195d7440D459fb0D36c95FC9D7B09CeBf3D54b2);
+        return IEntryPoint(0x6Da3712654481dF698faEDe14063fF79522ab6c5);
     }
 
     /**
