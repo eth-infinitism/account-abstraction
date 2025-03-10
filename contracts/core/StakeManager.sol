f392383ae8c46a9b05ec7039be7fdb7f8ce74216
Alex Forshtat <forshtat1@gmail.com>
2025-03-10 16:40:42 +0100

    Fix typos

--- contracts/core/StakeManager.sol
+++ contracts/core/StakeManager.sol
@@ -60,9 +60,9 @@ abstract contract StakeManager is IStakeManager {
     }
 
     /**
-     * Try to dncrement the account's deposit.
-     * @param account - The account to increment.
-     * @param amount  - The amount to increment by.
+     * Try to decrement the account's deposit.
+     * @param account - The account to decrement.
+     * @param amount  - The amount to decrement by.
      * @return true if the decrement succeeded (that is, previous balance was at least that amount)
      */
     function _tryDecrementDeposit(address account, uint256 amount) internal returns(bool) {
