1e721c935d8fd140aeb8d6e6917caec33bb4d48e
Minebuu <80020902+minebuu@users.noreply.github.com>
2025-03-14 15:40:05 +0900

    Use SIG_VALIDATION_SUCCESS and update return value on verification failure in isValidSignature()
    1. Changed the return value `0` to `SIG_VALIDATION_SUCCESS` on successful signature verification in `_validateSignature()`.  
    
    2. Updated the return value in `isValidSignature()` on verification failure from `0` to `0xffffffff` (referencing ERC-1271 reference implementation code).

--- contracts/accounts/Simple7702Account.sol
+++ contracts/accounts/Simple7702Account.sol
@@ -29,11 +29,11 @@ contract Simple7702Account is BaseAccount, IERC165, IERC1271, ERC1155Holder, ERC
         bytes32 userOpHash
     ) internal virtual override returns (uint256 validationData) {
 
-        return _checkSignature(userOpHash, userOp.signature) ? 0 : SIG_VALIDATION_FAILED;
+        return _checkSignature(userOpHash, userOp.signature) ? SIG_VALIDATION_SUCCESS : SIG_VALIDATION_FAILED;
     }
 
     function isValidSignature(bytes32 hash, bytes memory signature) public view returns (bytes4 magicValue) {
-        return _checkSignature(hash, signature) ? this.isValidSignature.selector : bytes4(0);
+        return _checkSignature(hash, signature) ? this.isValidSignature.selector : 0xffffffff;
     }
 
     function _checkSignature(bytes32 hash, bytes memory signature) internal view returns (bool) {
