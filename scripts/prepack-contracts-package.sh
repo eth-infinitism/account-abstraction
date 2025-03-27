4bb043f7cc9276ed9e13e0f661520efeec74319e
Darya <127833442+defiberrys@users.noreply.github.com>
2025-03-27 10:19:35 +0300

    chore: improved shell script for contract packaging and file handling

--- scripts/prepack-contracts-package.sh
+++ scripts/prepack-contracts-package.sh
@@ -1,17 +1,22 @@
 #!/bin/bash -xe
 #echo prepack for "contracts" package
 
-cd `dirname $0`/..
+cd $(dirname $0)/..
 pwd
-if git status contracts | grep -v 'nothing to commit'|tee /dev/stderr |grep -q Untracked; then
+
+if git status contracts | grep -v 'nothing to commit' | grep -q Untracked; then
   exit 1
 fi
 
 yarn clean
 yarn compile
+
 cd contracts
 
 rm -rf artifacts
 
 mkdir -p artifacts
-cp `find  ../artifacts/contracts -type f | grep -v -E 'test|Test|dbg|bls|IOracle|v06'` artifacts/
+
+find ../artifacts/contracts -type f | grep -v -E 'test|Test|dbg|bls|IOracle|v06' | while IFS= read -r file; do
+  cp "$file" artifacts/
+done
