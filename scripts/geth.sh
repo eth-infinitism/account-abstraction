1e5bf8262fa52db32c7e542175b8cabf4a7b054f
Fletcher Vaughn <fletchervaughn@gmail.com>
2026-08-13 21:53:15 -0400

    Fix local geth dev script and bump 7702 test gas limit
    Quote --http.corsdomain/--http.vhosts wildcards in scripts/geth.sh so
    bash doesn't glob-expand them to repo filenames before reaching
    docker run. Bump verificationGasLimit in the EIP-7702 entrypoint test
    so the delegate-init flow doesn't run out of gas.

--- scripts/geth.sh
+++ scripts/geth.sh
@@ -3,5 +3,6 @@ name=geth-$$
 trap "echo killing docker; docker kill $name 2> /dev/null" EXIT
 port=$1
 shift
-params="--http --http.api eth,net,web3,debug --rpc.allow-unprotected-txs --allow-insecure-unlock --dev --http.addr 0.0.0.0"
-docker run --name $name --rm -p $port:8545 ethpandaops/geth:master $params
+docker run --name $name --rm -p $port:8545 ethpandaops/geth:master \
+  --http --http.api eth,net,web3,debug --rpc.allow-unprotected-txs --dev --http.addr 0.0.0.0 \
+  --http.corsdomain '*' --http.vhosts '*'
